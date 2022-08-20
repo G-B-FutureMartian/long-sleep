d3c0203cf51a54c1ffbb8d0f1657d22c340db547
G-B-FutureMartian <81188293+G-B-FutureMartian@users.noreply.github.com>
2022-08-20 08:54:30 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from time import sleep
+
+def setTimer(hours, mins, secs):
+	hours
+
+if __name__ == "__main__":
+	print("Testing")
+	print("Please set timer(hours).")
+	hours = input()
+	print("Please set timer(minutes).")
+	mins = input()
+	print("Please set timer(seconds).")
+	secs = input()
+	timerTime = timerTime*60
